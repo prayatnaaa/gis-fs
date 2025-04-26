e6b4a31228d7b87aafadc2d8e66646d8c4b7852b
Tude Prayatna <tudeprayatna04@gmail.com>
2025-04-26 11:54:42 +0800

    feat(add): create tables for polyline

---
+++ app/Http/Controllers/PolylineController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Polyline;
+use Illuminate\Http\Request;
+
+class PolylineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Polyline $polyline)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Polyline $polyline)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Polyline $polyline)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Polyline $polyline)
+    {
+        //
+    }
+}
